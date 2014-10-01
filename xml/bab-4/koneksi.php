98edc2c66ab3e17077c334220df0f0f4075d0f7d
hasancodet <efendi.hasan@mail.ugm.ac.id>
2014-10-01 08:45:22 +0700

    ini sudah jalan

--- xml/bab-4/koneksi.php
+++ xml/bab-4/koneksi.php
@@ -1,6 +1,6 @@
 <?php
-  // lomelso database
-$link = mysql_connect('127.0.0.1','root','') or die('Could not connect: '.mysql_error());
-mysql_select_db('module_sit') or die('Could not select database');
-
+	//koneksi database
+	$link = mysql_connect ('localhost','root','')
+	or die ('Could not connect' . mysql_error());
+	mysql_select_db('mahasiswa') or die ('Could not select database');
 ?>
